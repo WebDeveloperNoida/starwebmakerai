967d75fb78a3c3bdab6d052f151e5240e5bb8940
Web Developer Noida <yameenstarwebmaker@gmail.com>
2025-03-10 15:19:55 +0530

    download image commit

--- src/app/text-to-image/textimagegenerate.jsx
+++ src/app/text-to-image/textimagegenerate.jsx
@@ -22,7 +22,7 @@ function Textimagegenerate() {
       "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
       {
         headers: {
-          Authorization: "Bearer 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",
+          Authorization: process.env.HUGGING_FACE_TOKEN,
           "Content-Type": "application/json",
         },
         method: "POST",
